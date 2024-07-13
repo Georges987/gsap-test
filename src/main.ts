d8610909fab05f620ed8ffd66a68837c4c98445d
= <=>
2024-07-13 15:30:45 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import '@/assets/css/style.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(PrimeVue, {
+        unstyled: true
+    })
+    .mount('#app')
